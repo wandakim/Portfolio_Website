3c595ee14a468b397cb0255ad268012f6bece859
wandakim <nwd1583@naver.com>
2021-04-15 15:18:22 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -41,10 +41,26 @@ document.addEventListener('scroll', () => {
     } 
 })
 
+// Arrow up
+const arrowUp = document.querySelector('.arrow-up')
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2){
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible')
+    }   
+})
 
+//Handle click on the "arrow up" button
 
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
+
+
+
